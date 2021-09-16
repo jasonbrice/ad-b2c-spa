590aeff891729907594f4a112ae8ef87be3c7df2
Jason Brice <Jason.Brice@amba.info>
2021-09-16 14:36:06 -0500

    altered config to use correct values

--- App/apiConfig.js
+++ App/apiConfig.js
@@ -1,5 +1,5 @@
 // The current application coordinates were pre-registered in a B2C tenant.
 const apiConfig = {
-    b2cScopes: ["https://fabrikamb2c.onmicrosoft.com/helloapi/demo.read"],
+    b2cScopes: ["https://ambaguest.onmicrosoft.com/eeff946f-5d1a-43db-885f-bbb4f662a848/User.Read"],
     webApi: "https://fabrikamb2chello.azurewebsites.net/hello"
   };
--- App/policies.js
+++ App/policies.js
@@ -5,16 +5,16 @@
  */
 const b2cPolicies = {
     names: {
-        signUpSignIn: "B2C_1_susi_reset_v2",
-        editProfile: "B2C_1_edit_profile_v2"
+        signUpSignIn: "B2C_1_signupsignin1",
+        editProfile: "B2C_1_edit_profile"
     },
     authorities: {
         signUpSignIn: {
-            authority: "https://fabrikamb2c.b2clogin.com/fabrikamb2c.onmicrosoft.com/B2C_1_susi_reset_v2",
+            authority: "https://ambaguest.b2clogin.com/ambaguest.onmicrosoft.com/B2C_1_signupsignin1",
         },
         editProfile: {
-            authority: "https://fabrikamb2c.b2clogin.com/fabrikamb2c.onmicrosoft.com/B2C_1_edit_profile_v2"
+            authority: "https://ambaguest.b2clogin.com/ambaguest.onmicrosoft.com/B2C_1_edit_profile"
         }
     },
-    authorityDomain: "fabrikamb2c.b2clogin.com"
+    authorityDomain: "ambaguest.b2clogin.com"
 }
